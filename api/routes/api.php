f5cb29e1dcf5d6321406a40f02e5ff1a9ee59221
Fabian Mieller <kontakt@fabianmieller.de>
2017-12-01 12:49:59 +0100

    feat | create controller and routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -12,3 +12,36 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Check if user is authenticated
+Route::group(['middleware' => 'auth:api'], function() {
+	// Article Routes
+	Route::get('articles', function() {
+			// Get all articles
+	});
+	Route::get('articles/{id}', function() {
+			// Get article by id
+	});
+	Route::post('articles', function() {
+			//
+	});
+	Route::put('articles/{id}', function() {
+			// Update article by id
+	});
+	Route::delete('articles/{id}', function() {
+			// Delete article by id
+	});
+});
+
+// Auth Routes
+Route::post('register', function() {
+		// Register user
+});
+
+Route::post('login', function() {
+		// Login user
+});
+
+Route::post('logout', function() {
+		// Logout user
+});
